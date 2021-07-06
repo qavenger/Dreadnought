7911b3d50e9288d75eafdb9a4795d07df8c4073d
XQue <qavenger@hotmail.com>
2021-07-07 02:01:01 +0800

    Updated - Templated function classes

--- Dreadnought/Source/Engine/Abstract/TemplatedBase.h
+++ Dreadnought/Source/Engine/Abstract/TemplatedBase.h
@@ -1,5 +1,65 @@
 #pragma once
 #include <set>
+template<typename RetType, typename... Args>
+class TFunctionBase
+{
+public:
+	virtual RetType operator()(Args...) = 0;
+};
+
+template<typename Class, typename RetType, typename... Args>
+class TMemberFunction : public TFunctionBase<RetType, Args...>
+{
+public:
+	TMemberFunction(Class* pObj, RetType(Class::* f)(Args...))
+		: pObj(pObj), pFunc(f){}
+	virtual RetType operator()(Args... args)override final
+	{
+		(pObj->*pFunc)(args...);
+	}
+protected:
+	Class* pObj;
+	RetType(Class::* pFunc)(Args...);
+};
+
+template<typename RetType, typename... Args>
+class TFunction : public TFunctionBase<RetType, Args...>
+{
+public:
+	TFunction(RetType(*pFunc)(Args...)) :pFunc(pFunc) {}
+	virtual RetType operator()(Args... args)override final
+	{
+		pFunc(args...);
+	}
+protected:
+	RetType(*pFunc)(Args...);
+};
+
+//template<size_t NumArgs, typename...Args>
+//class TMulticastDelegate
+//{
+//public:
+//	static constexpr size_t ArgCount = sizeof ...(Args);
+//	TMulticastDelegate() { static_assert(NumArgs == ArgCount); }
+//	~TMulticastDelegate() { 
+//		for (auto o : pFuncs) 
+//		{ 
+//			delete o; 
+//		} 
+//		pFuncs.clear(); 
+//	}
+//	template<size_t i>
+//	struct ArgTypes
+//	{
+//		typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+//	};
+//
+//#define Arg(x) void(*x)(Args...)
+//	void Bind(Arg(f)) { pFuncs.insert(new TFunction(f)); }
+//#undef Arg
+//private:
+//	std::set< TFunctionBase<void, Args...>* > pFuncs;
+//};
 
 template<typename... Args>
 class TMulticastDelegate
